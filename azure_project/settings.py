fa0b4b47bb0cd45d8c76d360c69c1e54aec548e7
adepentane <adepentane5@gmail.com>
2023-12-23 10:13:18 +0100

    updated Crispy Forms

--- azure_project/settings.py
+++ azure_project/settings.py
@@ -99,7 +99,8 @@ USE_I18N = True
 
 USE_TZ = True
 
-CRISPY_TEMPLATE_PACK ='bootstrap4'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
